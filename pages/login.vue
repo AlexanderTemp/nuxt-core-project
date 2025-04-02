11de57d2e50d397c42e0bde058cabe005fbe585c
Alexander Nina <alexander.nina@agetic.gob.bo>
2025-04-02 13:35:58 -0400

    fix(consumidor): actualización de diseño de login

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts"></script>
 <template>
   <div class="min-h-screen w-full grid place-items-center">
-    <UCard class="min-h-[70vh]">
-      <div class="grid lg:grid-cols-2">
+    <UCard
+      class="h-[70vh] w-[70vw]"
+      :ui="{
+        body: '!p-2'
+      }"
+    >
+      <div class="grid lg:grid-cols-2 gap-4">
         <div
-          class="w-full m-2 rounded-sm bg-[url(~/assets/img/login_1.jpg)] relative"
+          class="w-full h-full rounded-sm bg-[url(~/assets/img/login_1.jpg)] relative"
         />
+        <div class="flex flex-col justify-between">
+          <h1>Login</h1>
+
+          <d class="flex">
+            <p>Cuerpo</p>
+          </d>
+
+          <d class="flex">
+            <UButton>Iniciar sesión</UButton>
+          </d>
+        </div>
       </div>
     </UCard>
   </div>
