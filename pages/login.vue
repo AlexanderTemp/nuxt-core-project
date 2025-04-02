58ec5327308af5d98e8c66c53be08a8f42611b77
Alexander Nina <alexander.nina@agetic.gob.bo>
2025-04-02 14:03:01 -0400

    style(login): clip path added to get the specific shape for the login image

--- pages/login.vue
+++ pages/login.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="min-h-screen w-full grid place-items-center">
+  <div class="bg-amber-100 min-h-screen w-full grid place-items-center">
     <UCard
-      class="h-[70vh] w-[70vw]"
+      class="min-h-[500px] h-[70vh] w-[60vw]"
       :ui="{
-        body: '!p-2'
+        body: 'h-full !p-2 '
       }"
     >
-      <div class="grid lg:grid-cols-2 gap-4">
+      <div class="h-full grid lg:grid-cols-12 gap-4">
         <div
-          class="w-full h-full rounded-sm bg-[url(~/assets/img/login_1.jpg)] relative"
+          class="lg:col-span-7 relative bg-[url(~/assets/img/login_1.jpg)] img-shape bg-cover rounded-md"
         />
-        <div class="flex flex-col justify-between">
+        <div class="w-[80%] lg:col-span-5 flex flex-col justify-between">
           <h1>Login</h1>
 
           <d class="flex">
             <p>Cuerpo</p>
           </d>
 
-          <d class="flex">
-            <UButton>Iniciar sesión</UButton>
-          </d>
+          <UButton>Iniciar sesión</UButton>
         </div>
       </div>
     </UCard>
   </div>
 </template>
+
+<style lang="css" scoped>
+.img-shape {
+  background-position: center;
+  clip-path: polygon(0 0, 100% 0, 92% 100%, 0% 100%);
+}
+</style>
