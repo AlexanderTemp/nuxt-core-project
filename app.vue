9a953769a2fbbda840e1884df1b8d38fe7841982
Alexander Nina <alexander.nina@agetic.gob.bo>
2025-04-02 13:21:23 -0400

    feat: initial skeleton structure

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Toaster from '@nuxt/ui/runtime/components/Toaster.vue';
+</script>
+<template>
+  <UApp>
+    <NuxtLayout name="default">
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+      <Toaster position="top-center" :expand="true" />
+    </NuxtLayout>
+  </UApp>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="min-h-screen w-full grid place-items-center">
+    <UCard class="min-h-[70vh]">
+      <div class="grid lg:grid-cols-2">
+        <div
+          class="w-full m-2 rounded-sm bg-[url(~/assets/img/login_1.jpg)] relative"
+        />
+      </div>
+    </UCard>
+  </div>
+</template>
